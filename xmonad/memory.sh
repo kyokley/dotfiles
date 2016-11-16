c7b60e3cd4243e3a424ee6979dda8d961ccbb37d
Kevin Yokley <kyokley2@gmail.com>
2016-11-16 16:30:44 -0600

    Supporting new free method

--- xmonad/memory.sh
+++ xmonad/memory.sh
@@ -12,11 +12,11 @@ function float_cond()
     return $stat
 }
 
-mem_stats=$(free | grep buffers | grep -Po '^\D+\d+\s+\d+' | grep -Po '\d+\s+\d+$')
-used=$(echo $mem_stats | grep -Po '^\d+')
-free=$(echo $mem_stats | grep -Po '\d+$')
+mem_stats=$(free | grep 'Mem')
+used=$(echo $mem_stats | awk '{print $3}')
+total=$(echo $mem_stats | awk '{print $2}')
 
-mem_val=$(echo "$used $free" | python -c "print str(int(100 * round(reduce(lambda x, y: float(x) / float(int(x) + int(y)), raw_input().split(' ')), 3)))")
+mem_val=$(echo "$used $total" | python -c "print str(int(100 * round(reduce(lambda x, y: float(x) / float(y), raw_input().split(' ')), 3)))")
 
 if float_cond "$mem_val < 35"; then
     temp="Mem: <fc=#00ff00>${mem_val}</fc>%"
