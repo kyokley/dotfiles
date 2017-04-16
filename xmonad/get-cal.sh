dcd78ced60803b7af068c917a62144333854a3d6
Kevin Yokley <kyokley2@gmail.com>
2017-04-16 07:11:57 -0500

    Adding get-cal.sh

--- xmonad/get-cal.sh
+++ xmonad/get-cal.sh
@@ -1,3 +1,3 @@
 #1/bin/bash
 
-python -c "import random; print random.choice([x for x in '''$(gcalcli agenda --nocolor --nostarted "$(date)" "$(date | awk '$4 = "23:59:59"')")'''.split('\n') if x])" | sed -r 's/\s+/ /g' | sed 's/No Events Found.../No Events/'
+python -c "import random; print random.choice([x for x in '''$(gcalcli --prefix '%a %b %d' --nocolor agenda "$(date)" "$(date | awk '$4 = "23:59:59"')")'''.split('\n') if x])" | sed -r 's/\s+/ /g' | sed 's/No Events Found.../No Events/'
