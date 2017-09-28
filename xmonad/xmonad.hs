eb23086bc854ca7eeca3bf4da75f72829bf9c9c4
Kevin Yokley <kyokley2@gmail.com>
2017-09-28 10:03:14 -0500

    Updating xmonad.hs to handle dock

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -164,7 +164,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
     --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+    , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
@@ -381,7 +381,7 @@ myStartupHook = do
 --
 main = do
   xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
-  xmonad $ withUrgencyHook NoUrgencyHook $ ewmh defaultConfig {
+  xmonad $ withUrgencyHook NoUrgencyHook $ ewmh $ docks def {
     focusedBorderColor = myFocusedBorderColor
   , focusFollowsMouse  = myFocusFollowsMouse
   , normalBorderColor = myNormalBorderColor
