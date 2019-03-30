34ce378812b78958409536717bf6d531da62e774
Kevin Yokley <kyokley2@gmail.com>
2019-03-30 18:16:39 -0500

    Remove unused submodules

--- xmonad/world_clock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-AUS="AUS: `env TZ=Australia/Melbourne date +'%a %b %_d %H:%M'`"
-UK="UK: `env TZ=UTC date +'%a %b %_d %H:%M'`"
-timezones=("$AUS" "$UK")
-
-timezone=${timezones[$(($RANDOM%${#timezones[*]}))]}
-echo $timezone
