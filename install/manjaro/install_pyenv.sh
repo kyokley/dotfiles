30df4f224fe360d6f7ef684daa1c5ad5ee8410e4
Kevin Yokley <kyokley2@gmail.com>
2020-04-20 11:15:18 -0500

    Include pyenv updater plugin in install scripts

--- install/manjaro/install_pyenv.sh
+++ install/manjaro/install_pyenv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PY27='2.7.15'
-PY3='3.6.7'
+PY3='3.8.2'
 
 if [ ! -h $HOME/.pyenv ]; then
     git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
@@ -11,25 +10,22 @@ if [ ! -h $HOME/.pyenv/plugins/pyenv-virtualenv ]; then
     git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
 fi
 
+if [ ! -h $HOME/.pyenv/plugins/pyenv-update ]; then
+    git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
+fi
+
 export PATH="$HOME/.pyenv/bin:$PATH"
-export PYENV_VERSION="$PY27"
+export PYENV_VERSION="$PY3"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-pyenv install $PY27
 pyenv install $PY3
 
-pyenv virtualenv $PY27 neovim2
 pyenv virtualenv $PY3 neovim3
-
-pyenv shell neovim2
-pip install pip --upgrade
-pip install python-language-server[all] neovim pip pyflakes flake8 bandit --upgrade
-pyenv which python  # Note the path
 
 pyenv shell neovim3
 pip install pip --upgrade
 pip install python-language-server[all] neovim pip pyflakes flake8 bandit black --upgrade
 pyenv which python  # Note the path
 
-pyenv global $PY3 $PY27
+pyenv global $PY3
