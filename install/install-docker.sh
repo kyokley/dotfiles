0f88a06188722bc5c35d4f002f11d2f7002e8935
Kevin Yokley <kyokley2@gmail.com>
2023-11-23 08:10:59 -0600

    Actually install docker

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -23,6 +23,8 @@ then
     "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 sudo groupadd docker
