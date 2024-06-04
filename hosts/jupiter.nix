289af0f27dbf8fa013b65d60d29f13f1b35be7b1
Kevin Yokley <kyokley2@gmail.com>
2024-06-04 06:48:02 -0500

    Recreate mc-run cmd for jupiter

--- hosts/jupiter.nix
+++ hosts/jupiter.nix
@@ -1,4 +1,15 @@
+{lib, ...}:
 {
   programs.nixvim.enable = false;
   programs.git.userEmail = "kyokley@jupiter";
+
+  programs.zsh.prezto.extraConfig = [
+        ''
+        function mc-run() {
+            cd ~/workspace/MediaConverterProd
+            make mc-run
+            cd -
+        }
+        ''
+  ];
 }
