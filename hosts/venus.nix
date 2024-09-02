182c11fa0feb0380bd8d6a28029f8076f722dfba
Kevin Yokley <kyokley2@gmail.com>
2024-09-01 23:05:07 -0500

    Remove neovim entry on venus

--- hosts/venus.nix
+++ hosts/venus.nix
@@ -1,9 +1,4 @@
 {
   programs.systemd-services.environment = "venus";
   programs.git.userEmail = "kyokley@venus";
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-  };
 }
