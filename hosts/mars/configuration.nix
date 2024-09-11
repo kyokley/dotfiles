e28e75b081ac91979e426d6ad356b11d6a68929a
Kevin Yokley <kyokley@mars>
2024-09-11 18:41:32 -0500

    Move sound setting into mars nixos config

--- hosts/mars/configuration.nix
+++ hosts/mars/configuration.nix
@@ -17,6 +17,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  sound.enable = true;
+
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
