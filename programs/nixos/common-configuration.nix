216bac78edc3921ddf6d8b3f82dde120d1a271c3
Kevin Yokley <kyokley@mercury>
2024-09-11 20:07:26 -0500

    Set alsa persistence

--- programs/nixos/common-configuration.nix
+++ programs/nixos/common-configuration.nix
@@ -67,6 +67,7 @@
   services.blueman.enable = true;
 
   # Enable sound with pipewire.
+  hardware.alsa.enablePersistence = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
