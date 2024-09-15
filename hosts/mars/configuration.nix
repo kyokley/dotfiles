da5f7e3eccdae15f1e71956eec18ad4fe93e2694
Kevin Yokley <kyokley@mars>
2024-09-15 16:01:23 -0500

    Switch to version 24.05 from unstable

--- hosts/mars/configuration.nix
+++ hosts/mars/configuration.nix
@@ -20,5 +20,5 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  hardware.alsa.enablePersistence = true;
+  sound.enable = true;
 }
