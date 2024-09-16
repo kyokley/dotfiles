38352cde5217a07dc9f262facfb444d5cf5d173e
Kevin Yokley <kyokley@mars>
2024-09-15 21:00:49 -0500

    Set hom stateVersion for venus

--- home.nix
+++ home.nix
@@ -21,15 +21,6 @@
 
   programs.systemd-services.enable = lib.mkDefault true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
--- hosts/venus.nix
+++ hosts/venus.nix
@@ -5,4 +5,6 @@
 
   programs.systemd-services.environment = "venus";
   programs.git.userEmail = "kyokley@venus";
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
 }
