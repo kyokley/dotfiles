998be75a539d177aaa190f889bce91ee6ac1b83d
Kevin Yokley <kyokley@mars>
2024-10-05 08:56:49 -0500

    Set home.stateVersion for singularity

--- hosts/singularity.nix
+++ hosts/singularity.nix
@@ -6,4 +6,6 @@
   programs.systemd-services.environment = "singularity";
 
   programs.git.userEmail = "kyokley@singularity";
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
 }
