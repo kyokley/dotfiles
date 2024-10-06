bc09157477ee0a99b519e3ae1185a9b37a361937
Kevin Yokley <kyokley@mars>
2024-10-06 06:14:05 -0500

    Set terminator titlebar font and size

--- programs/terminator.nix
+++ programs/terminator.nix
@@ -1,12 +1,19 @@
 {
   programs.terminator = {
     enable = true;
-    config = {
-      profiles.default.font = "Hack Nerd Font Mono 16";
-      profiles.default.use_system_font = false;
-      profiles.default.scrollback_infinite = true;
-      profiles.demo.font = "Hack Nerd Font Mono 25";
-      profiles.demo.use_system_font = false;
+    config.profiles = {
+      default = {
+        font = "Hack Nerd Font Mono 16";
+        use_system_font = false;
+        scrollback_infinite = true;
+        show_titlebar = true;
+        title_font = "Hack Nerd Font Mono 14";
+        title_use_system_font = false;
+      };
+      demo = {
+        font = "Hack Nerd Font Mono 25";
+        use_system_font = false;
+      };
     };
   };
 }
