48949c7a51942d5fb6a52e00a0daf85f84972278
Kevin Yokley <kevin.yokley@oracle.com>
2024-11-25 08:47:52 -0600

    Add script to activate single screen in qtile

--- hosts/saturn/saturn.nix
+++ hosts/saturn/saturn.nix
@@ -1,6 +1,9 @@
 { pkgs, nixvim, picom, ... }:
 let
     home_dir = "/home/yokley";
+    qtile-one-screen = pkgs.writeShellScriptBin "qtile-one-screen" ''
+        xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off
+    '';
     qtile-two-screen = pkgs.writeShellScriptBin "qtile-two-screen" ''
         xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
     '';
@@ -22,6 +25,7 @@ in
         pkgs.brightnessctl
         pkgs.scrot
         nixvim.packages.${pkgs.stdenv.hostPlatform.system}.default
+        qtile-one-screen
         qtile-two-screen
         qtile-three-screen
     ];
