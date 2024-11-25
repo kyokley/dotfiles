93ecd0e3f0682f8960c0a88bc673d8e6c892b125
Kevin Yokley <kyokley2@gmail.com>
2024-11-24 21:38:30 -0600

    Create picom toggle script

--- programs/qtile/toggle_picom.sh
+++ programs/qtile/toggle_picom.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-PID=$(ps ax | grep picom | grep -v toggle_picom | grep -v vim | grep -v grep | awk '{print $1}')
+PID=$(pgrep picom)
 RETURN_CODE=$?
 
 if [ $RETURN_CODE -eq 0 ] && [ "$PID" != "" ]
 then
     echo "PID: $PID RETURN_CODE: $RETURN_CODE"
-    kill $PID
+    kill "$PID"
 else
     export PYTHONDONTWRITEBYTECODE=1
     export PYTHONUNBUFFERED=1
