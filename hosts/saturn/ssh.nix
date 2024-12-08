7ae2ebfe0e82f7bc9bd8847567dc610169eea0c4
Kevin Yokley <kyokley@mars>
2024-12-08 09:30:58 -0600

    Re-add listenAddresses

--- hosts/saturn/ssh.nix
+++ hosts/saturn/ssh.nix
@@ -1,8 +1,14 @@
 {
   services.openssh = {
     enable = true;
-    ports = [
-        10101
+    # ports = [
+        # 10101
+    # ];
+    listenAddresses = [
+      {
+        addr = "192.168.50.96";
+        port = 10101;
+      }
     ];
     settings = {
       PermitRootLogin = "no";
