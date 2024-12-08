8bbfdb276190b80fa9a9c0401ae1ebe23376e8cb
Kevin Yokley <kyokley@mars>
2024-12-08 11:06:39 -0600

    Add fail2ban for ssh

--- hosts/saturn/ssh.nix
+++ hosts/saturn/ssh.nix
@@ -15,4 +15,25 @@
       PasswordAuthentication = false;
     };
   };
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    ignoreIP = [
+      "192.168.50.0/24"
+    ];
+    bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h";
+      overalljails = true;
+    };
+    jails = {
+      sshd.settings = {
+        enabled = true;
+      };
+    };
+  };
 }
