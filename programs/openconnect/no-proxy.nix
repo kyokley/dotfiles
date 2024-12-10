1098959fc5394e498c995bc2fece6df716e41ec2
Kevin Yokley <kevin.yokley@oracle.com>
2024-12-10 12:21:09 -0600

    Add domains

--- programs/openconnect/no-proxy.nix
+++ programs/openconnect/no-proxy.nix
@@ -22,9 +22,12 @@ let
           "odo-docker-local.artifactory.oci.oraclecorp.com"
           "oim.oraclecorp.com"
           "printers.oraclecorp.com"
+          "www-proxy-ash7.us.oracle.com"
           "www-proxy-adcq7-new.us.oracle.com"
-          "www-proxy-sjc.oraclecorp.com"
+          "www-proxy-adcq7.us.oracle.com"
           "www-proxy-hqdc.us.oracle.com"
+          "www-proxy-sjc.oraclecorp.com"
+          "exchange.oraclecorp.com"
   ];
 in
 {
@@ -72,12 +75,15 @@ in
     100.114.94.31 hrservices.oraclecorp.com         # vpn-slice-openconnect0 AUTOCREATED
     100.77.38.58 ocitpmpypi.us.oracle.com ocitpmpypi                # vpn-slice-openconnect0 AUTOCREATED
     144.25.81.188 ocp.oraclecorp.com                # vpn-slice-openconnect0 AUTOCREATED
-    100.126.4.64 odo-docker-local.artifactory.oci.oraclecorp.com            # vpn-slice-openconnect0 AUTOCREATED
     100.126.5.8 odo-docker-local.artifactory.oci.oraclecorp.com             # vpn-slice-openconnect0 AUTOCREATED
+    100.126.4.64 odo-docker-local.artifactory.oci.oraclecorp.com            # vpn-slice-openconnect0 AUTOCREATED
     100.112.14.9 oim.oraclecorp.com         # vpn-slice-openconnect0 AUTOCREATED
     100.112.125.102 printers.oraclecorp.com         # vpn-slice-openconnect0 AUTOCREATED
+    10.23.226.53 www-proxy-ash7.us.oracle.com www-proxy-ash7                # vpn-slice-openconnect0 AUTOCREATED
     10.23.226.53 www-proxy-adcq7-new.us.oracle.com www-proxy-adcq7-new              # vpn-slice-openconnect0 AUTOCREATED
-    10.255.48.38 www-proxy-sjc.oraclecorp.com               # vpn-slice-openconnect0 AUTOCREATED
+    10.23.226.53 www-proxy-adcq7.us.oracle.com www-proxy-adcq7              # vpn-slice-openconnect0 AUTOCREATED
     10.68.69.53 www-proxy-hqdc.us.oracle.com www-proxy-hqdc         # vpn-slice-openconnect0 AUTOCREATED
+    10.255.48.38 www-proxy-sjc.oraclecorp.com               # vpn-slice-openconnect0 AUTOCREATED
+    144.25.106.166 exchange.oraclecorp.com          # vpn-slice-openconnect0 AUTOCREATED
     '';
 }
