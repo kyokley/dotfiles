c0b5a62a9ae6393822d4c86afe56d3c227aa80fe
Kevin Yokley <kevin.yokley@oracle.com>
2024-12-10 16:48:03 -0600

    Set ipu6 for saturn

--- hosts/saturn/hardware-configuration.nix
+++ hosts/saturn/hardware-configuration.nix
@@ -37,4 +37,8 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6epmtl";
+  };
 }
