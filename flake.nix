e60911ae5c4b844bc9e62507a4bf2f416ab64a62
Kevin Yokley <kyokley@mars>
2024-12-11 11:41:25 -0600

    Fix nixpkg url

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/24.11-beta";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
