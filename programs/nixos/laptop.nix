a7d3a3551fc1490a6d72ce1c58cc45a6f672d048
Kevin Yokley <kyokley2@gmail.com>
2025-01-04 21:15:08 -0600

    Suspend laptop on lid close

--- programs/nixos/laptop.nix
+++ programs/nixos/laptop.nix
@@ -18,15 +18,15 @@ in
                 if [ $(DISPLAY=:0 ${pkgs.sudo}/bin/sudo -u ${username} ${pkgs.xorg.xrandr}/bin/xrandr | grep -P '\d+x\d+\+\d+\+\d+' | wc -l) = "1" ]; then
                     lid_state=$(cat /proc/acpi/button/lid/LID0/state | ${pkgs.gawk}/bin/awk '{print $NF}')
                     if [ $lid_state = "closed" ]; then
-                        systemctl suspend
                         DISPLAY=:0 ${pkgs.sudo}/bin/sudo -u ${username} ${pkgs.betterlockscreen}/bin/betterlockscreen --lock -- --nofork
+                        systemctl suspend
                     fi
                 fi
             '';
             powerEventCommands =
             ''
-                systemctl suspend
                 DISPLAY=:0 ${pkgs.sudo}/bin/sudo -u ${username} ${pkgs.betterlockscreen}/bin/betterlockscreen --lock -- --nofork
+                systemctl suspend
             '';
         };
     };
