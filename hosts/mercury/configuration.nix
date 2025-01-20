12b415984e0e64b451856bd77bc52cfba47f52f4
Kevin Yokley <kyokley@mars>
2025-01-19 22:36:04 -0600

    Generate mattermost certs monthly on mercury

--- hosts/mercury/configuration.nix
+++ hosts/mercury/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [
@@ -26,4 +26,35 @@
   ];
 
   system.stateVersion = "24.05"; # Don't touch me!
+
+  systemd = {
+    services = {
+      renew-mattermost-cert = {
+        Unit.Description = "Renew Mattermost Cert";
+        Service = {
+          Type = "oneshot";
+          ExecStart = toString (
+              pkgs.writeShellScript "renew-mattermost-cert" ''
+              PATH=$PATH:${lib.makeBinPath [ pkgs.gnumake pkgs.sudo ]}
+              cd /home/yokley/workspace/mattermost && make renew-cert
+              ''
+              );
+        };
+      };
+    };
+    timers = {
+      renew-mattermost-cert = {
+        Unit = {
+            Description = "Renew Mattermost Cert";
+            After = [ "network.target" ];
+        };
+        Timer = {
+            OnCalendar = "monthly";
+            Persistent = true;
+            Unit = "renew-mattermost-cert.service";
+        };
+        Install.WantedBy = ["timers.target"];
+      };
+    };
+  };
 }
