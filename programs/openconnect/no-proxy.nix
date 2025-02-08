532f6d2618f192a74814084887568d9cfb40a4cb
Kevin Yokley <kevin.yokley@oracle.com>
2025-02-08 10:51:38 -0600

    Add REDSOCKS comment to allow identifying REDSOCKS iptable rules

--- programs/openconnect/no-proxy.nix
+++ programs/openconnect/no-proxy.nix
@@ -133,7 +133,7 @@
         ++ map (x: "iptables -t nat -A REDSOCKS -d " + x + " -j RETURN || true") reserved-ips
         ++ [
           "iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports ${redsocks-listen-port} || true"
-          "iptables -t nat -A PREROUTING -i docker0 -p tcp -j REDSOCKS --to-ports ${redsocks-listen-port} || true"
+          ''iptables -t nat -A PREROUTING -i docker0 -p tcp -j REDIRECT --to-ports ${redsocks-listen-port} -m comment --comment "REDSOCKS docker rule" || true''
         ]
         ++ map (x: "iptables -t nat -A OUTPUT -p tcp -d " + x + "/32 -j REDSOCKS || true") proxied_ips
       )
