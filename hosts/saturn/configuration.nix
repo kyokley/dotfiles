6e4b8a2db8d7ebebaa6a776622a12f01f0fc3463
Kevin Yokley <kyokley2@gmail.com>
2025-02-18 06:59:32 -0600

    Enable ipu6 camera patch

--- hosts/saturn/configuration.nix
+++ hosts/saturn/configuration.nix
@@ -24,7 +24,20 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = ["bcachefs"];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest.extend (self: super: {
+    ipu6-drivers = super.ipu6-drivers.overrideAttrs (
+      final: previous: rec {
+        src = builtins.fetchGit {
+          url = "https://github.com/intel/ipu6-drivers.git";
+          ref = "master";
+          rev = "b4ba63df5922150ec14ef7f202b3589896e0301a";
+        };
+        patches = [
+          "${src}/patches/0001-v6.10-IPU6-headers-used-by-PSYS.patch"
+        ];
+      }
+    );
+  });
 
   networking.hostName = "saturn"; # Define your hostname.
 
