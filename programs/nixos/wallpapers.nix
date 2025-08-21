aabef7154d8db1d7705b1556a18097fb932063a6
Kevin Yokley <kyokley@mars>
2025-08-20 19:40:02 -0500

    Use main for clonix source

--- programs/nixos/wallpapers.nix
+++ programs/nixos/wallpapers.nix
@@ -7,8 +7,8 @@
   remoteWallpaperHost = "jupiter";
   sync-wallpapers = pkgs.writeScriptBin "sync-wallpapers" ''
     set -x
-    ${pkgs.rsync}/bin/rsync -ruv ${remoteWallpaperHost}:${homeDir}/Pictures/wallpapers ${homeDir}/Pictures/wallpapers
-    ${pkgs.rsync}/bin/rsync -ruv ${homeDir}/Pictures/wallpapers ${remoteWallpaperHost}:${homeDir}/Pictures/wallpapers
+    ${pkgs.rsync}/bin/rsync -ruv ${remoteWallpaperHost}:${homeDir}/Pictures/wallpapers ${homeDir}/Pictures/
+    ${pkgs.rsync}/bin/rsync -ruv ${homeDir}/Pictures/wallpapers ${remoteWallpaperHost}:${homeDir}/Pictures/
   '';
 in {
   imports = [
@@ -24,15 +24,15 @@ in {
     deployments = [
       {
         deploymentName = "localToRemote";
-        local.dir = "yokley@jupiter:${homeDir}/Pictures/wallpapers";
-        targetDir = "${homeDir}/Pictures/wallpapers";
+        source.dir = "yokley@jupiter:${homeDir}/Pictures/wallpapers";
+        targetDir = "${homeDir}/Pictures/";
         remote.enable = false;
         remote.user.name = "yokley";
         remote.ipOrHostname = remoteWallpaperHost;
         timer.enable = true;
         timer.Persistent = true;
         timer.OnCalendar = "daily";
-        extraOptions = "--rsh=\"${pkgs.openssh}/bin/ssh -o 'StrictHostKeyChecking=no'\"";
+        extraOptions = "--timeout=10 --rsh=\"${pkgs.openssh}/bin/ssh -o 'StrictHostKeyChecking=no'\"";
       }
     ];
   };
