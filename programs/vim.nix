244ade02a0be8316e3f366e3fcbd898b7ccdee6f
Kevin Yokley <kyokley@mars>
2025-08-27 20:48:52 -0500

    Add docker vim command

--- programs/vim.nix
+++ programs/vim.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  docker-vim = pkgs.writeShellScriptBin "dvim" ''
+    docker run --rm -it -v $(pwd):/files -w /files kyokley/nixvim $@
+  '';
+in {
   home.packages = [
     pkgs.universal-ctags
+    docker-vim
   ];
+
   programs.git.extraConfig.core.editor = "vim";
 
   home.sessionVariables = {
