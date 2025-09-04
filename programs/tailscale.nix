9004bb66b6f7b0e13f3c9da33cb9387c527452c0
Kevin Yokley <kyokley@mars>
2025-09-03 19:17:03 -0500

    Install tailscale from unstable

--- programs/tailscale.nix
+++ programs/tailscale.nix
@@ -1,14 +1,21 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   # Enable tailscale
-  services.tailscale.enable = true;
-  services.tailscale.useRoutingFeatures = "both";
+  services.tailscale = {
+    enable = true;
+    package = pkgs-unstable.tailscale;
+    useRoutingFeatures = "both";
+  };
   networking.nameservers = [
     "100.124.31.71"
   ];
 
   # Enable trayscale once added to home-manager
   # services.trayscale.enable = false;
-  environment.systemPackages = with pkgs; [
-    tailscale
+  environment.systemPackages = [
+    pkgs-unstable.tailscale
   ];
 }
