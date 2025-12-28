2b603fc6d12d6bcaac53b89c05c02dd6305fb8fd
Kevin Yokley <kyokley@mars>
2025-12-27 20:11:00 -0600

    Set remote build protocol to ssh

--- hosts/mars/configuration.nix
+++ hosts/mars/configuration.nix
@@ -52,7 +52,7 @@
       hostName = "192.168.50.31";
       sshUser = "yokley";
       system = "x86_64-linux";
-      protocol = "ssh-ng";
+      protocol = "ssh";
       maxJobs = 3;
       speedFactor = 2;
       supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
