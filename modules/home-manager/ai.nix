8c8f763681b6df9af70ccfc98247da89349693b1
Kevin Yokley <kyokley@mars>
2026-01-16 11:17:35 -0600

    Set OLLAMA_HOST env var

--- modules/home-manager/ai.nix
+++ modules/home-manager/ai.nix
@@ -1,7 +1,8 @@
 {
   programs.aider-chat.enable = true;
   home.sessionVariables = {
-    OLLAMA_API_BASE = "http://100.92.134.123:11434";
+    OLLAMA_HOST = "100.92.134.123:11434";
+    OLLAMA_API_BASE = "http://${OLLAMA_HOST}";
     # NIXVIM_AIDER_MODEL = "ollama_chat/gpt-oss";
     NIXVIM_AIDER_MODEL = "ollama_chat/llama3.2:3b";
   };
