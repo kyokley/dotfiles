bd71e60157c695125bac222023092389f7eb9bf6
Kevin Yokley <kyokley2@gmail.com>
2026-04-18 07:03:59 -0500

    🔧 chore(venus)(main): restructure programs config and add direnv zsh integration
    - reorganize programs configuration into nested structure
    - add direnv hook initialization to zsh init content
    </output>

--- hosts/venus/home.nix
+++ hosts/venus/home.nix
@@ -8,7 +8,15 @@
     ../../modules/home-manager/home.nix
   ];
 
-  programs.git.settings.user.email = "kyokley@venus";
+  programs = {
+    git.settings.user.email = "kyokley@venus";
+
+    zsh = {
+      initContent = ''
+        eval "$(direnv hook zsh)"
+      '';
+    };
+  };
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
