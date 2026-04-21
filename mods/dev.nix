0668f5e3fd46b229c08a6e0d9e08d6049a777250
Kevin Yokley <kyokley2@gmail.com>
2026-04-21 11:02:21 -0500

    🏗️ refactor(flake)(main): migrate flake.nix configuration to modular structure
    - add import-tree dependency to flake.lock and flake.nix inputs
    - move main flake configuration logic into mods/base.nix module
    - create mods/dev.nix module for development environment packages
    - remove dev.nix imports from dioxygen and mars home configurations
    - delete modules/home-manager/dev.nix file

---
+++ mods/dev.nix
@@ -0,0 +1,39 @@
+{
+  perSystem = {
+    config,
+    pkgs,
+    ...
+  }: {
+    packages.dioxygen = {
+      home.packages = [
+        pkgs.gnumake
+        pkgs.ripgrep
+        pkgs.tig
+        pkgs.jq
+        pkgs.devenv
+        pkgs.direnv
+        pkgs.ragenix
+        pkgs.gh
+        # inputs.usql.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+
+      home.file = {
+        pdbpp = {
+          enable = true;
+          target = ".pdbrc.py";
+          text = ''
+            import pdb
+            class Config(pdb.DefaultConfig):
+                sticky_by_default = True
+          '';
+        };
+      };
+
+      programs.zsh = {
+        initContent = ''
+          eval "$(direnv hook zsh)"
+        '';
+      };
+    };
+  };
+}
