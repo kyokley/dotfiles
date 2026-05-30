bd2cb0e9a3e4da4e241a954d7ef00b16394be55c
Kevin Yokley <kyokley@mars>
2026-05-30 13:44:33 -0500

    🔧 chore(syncthing)(secrets): refactor syncthing secrets configuration to use generated attributes
    - add jupiter host public key
    - extract syncthing hosts into a reusable list (mars, dioxygen, venus)
    - replace repeated secret definitions with dynamically generated attributes
    - use builtins.listToAttrs and map to generate cert and key entries for each host
    - maintain same output structure with merged syncthing-certs and syncthing-keys attributes

--- modules/parts/_secrets/secrets.nix
+++ modules/parts/_secrets/secrets.nix
@@ -3,29 +3,38 @@ let
   mars = builtins.readFile ../../hosts/mars/mars.pub;
   dioxygen = builtins.readFile ../../hosts/dioxygen/dioxygen.pub;
   venus = builtins.readFile ../../hosts/venus/venus.pub;
-in {
-  "syncthing/mars/key.age" = {
-    publicKeys = [mars];
-    armor = true;
-  };
-  "syncthing/mars/cert.age" = {
-    publicKeys = [mars];
-    armor = true;
-  };
-  "syncthing/dioxygen/key.age" = {
-    publicKeys = [dioxygen];
-    armor = true;
-  };
-  "syncthing/dioxygen/cert.age" = {
-    publicKeys = [dioxygen];
-    armor = true;
-  };
-  "syncthing/venus/key.age" = {
-    publicKeys = [venus];
-    armor = true;
-  };
-  "syncthing/venus/cert.age" = {
-    publicKeys = [venus];
-    armor = true;
-  };
-}
+  jupiter = builtins.readFile ../../hosts/jupiter/jupiter.pub;
+
+  syncthing-hosts = [
+    mars
+    dioxygen
+    venus
+  ];
+
+  # This is a bit of a hack to avoid having to repeat the same code for each host. We can generate the attrs for each host and then merge them together.
+  # "syncthing/${host}/key.age" = {
+  #   publicKeys = [${host}];
+  #   armor = true;
+  # };
+  # "syncthing/${host}/cert.age" = {
+  #   publicKeys = [${host}];
+  #   armor = true;
+  # };
+  syncthing-vars = ["certs" "keys"];
+  syncthing-attrs = builtins.listToAttrs (map (var: {
+      name = "syncthing-${var}";
+      value = builtins.listToAttrs (map (host: {
+          name = "syncthing/${host}/cert.age";
+          value = {
+            publicKeys = [host];
+            armor = true;
+          };
+        })
+        syncthing-hosts);
+    })
+    syncthing-vars);
+in
+  syncthing-attrs.syncthing-certs
+  // syncthing-attrs.syncthing-keys
+  // {
+  }
