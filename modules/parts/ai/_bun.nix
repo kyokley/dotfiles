f70589ea308c43e81139b89601183221576a928a
Kevin Yokley <kyokley@mars>
2026-06-21 15:15:15 -0500

    🔧 chore(main)(deps): update oh-my-opencode-slim from 2.0.1 to 2.0.4

--- modules/parts/ai/_bun.nix
+++ modules/parts/ai/_bun.nix
@@ -972,9 +972,9 @@
     url = "https://registry.npmjs.org/object-inspect/-/object-inspect-1.13.4.tgz";
     hash = "sha512-W67iLl4J2EXEGTbfeHCffrjDfitvLANg0UlX3wFUUSTx92KXRFegMHUVgSqE+wvhAbi4WqjGg9czysTV2Epbew==";
   };
-  "oh-my-opencode-slim@2.0.1" = fetchurl {
-    url = "https://registry.npmjs.org/oh-my-opencode-slim/-/oh-my-opencode-slim-2.0.1.tgz";
-    hash = "sha512-KHesqUHswGATWyT5Xkhv9kWhrS6s4rTAOuNmUPCyFVq0SjoOvhzquRWErsKMRoBCZVIB8MsvAlwhLBHn2vBmag==";
+  "oh-my-opencode-slim@2.0.4" = fetchurl {
+    url = "https://registry.npmjs.org/oh-my-opencode-slim/-/oh-my-opencode-slim-2.0.4.tgz";
+    hash = "sha512-HNQUJN8Fa3r2flm3aT0Dtzfn5tiHBXAzeTz9UOGhcZsPRCHsxCHG3iSYvIhKoEuYnjHBSLXzANg/BQc5WfYAJQ==";
   };
   "omggif@1.0.10" = fetchurl {
     url = "https://registry.npmjs.org/omggif/-/omggif-1.0.10.tgz";
