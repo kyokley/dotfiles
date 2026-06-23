acb4b6059afa46e948868a5ad3a84e7131630ed6
Kevin Yokley <kyokley@mars>
2026-06-23 15:48:58 -0500

    🔧 chore(distributed-build): replace mercury tailscale builder with bangup.dyndns.org
    - add SSH host configuration for bangup.dyndns.org on port 10101
    - replace mercury.taila5201.ts.net remote builder with bangup.dyndns.org
    - set programs.ssh.extraConfig with the SSH host configuration
    - apply changes to both nixos and homeManager distributed build configs

--- modules/parts/distributed_builds.nix
+++ modules/parts/distributed_builds.nix
@@ -9,31 +9,44 @@
       speedFactor = 2;
       supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
     };
+
+    ssh_conf = ''
+      Host bangup.dyndns.org
+          HostName bangup.dyndns.org
+          Port 10101
+          StrictHostKeyChecking=accept-new
+    '';
   in {
     nixos.distributedBuilds = {username, ...}: {
       nix = {
         buildMachines = [
           # (mkMachine username "192.168.50.31")
-          (mkMachine username "mercury.taila5201.ts.net")
+          # (mkMachine username "mercury.taila5201.ts.net")
+          (mkMachine username "bangup.dyndns.org")
         ];
         distributedBuilds = true;
         extraOptions = ''
           builders-use-substitutes = true
         '';
       };
+
+      programs.ssh.extraConfig = ssh_conf;
     };
 
     homeManager.distributedBuilds = {username, ...}: {
       nix = {
         buildMachines = [
           # (mkMachine username "192.168.50.31")
-          (mkMachine username "mercury.taila5201.ts.net")
+          # (mkMachine username "mercury.taila5201.ts.net")
+          (mkMachine username "bangup.dyndns.org")
         ];
         distributedBuilds = true;
         extraOptions = ''
           builders-use-substitutes = true
         '';
       };
+
+      programs.ssh.extraConfig = ssh_conf;
     };
   };
 }
