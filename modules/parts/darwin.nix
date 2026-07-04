c28a03169dab243e55b41bce631896b510a4f271
Kevin Yokley <kyokley2@gmail.com>
2026-07-04 08:14:14 -0500

    ✨ feat(darwin)(nix): add Nix daemon and garbage collection configuration
    - set nixbld GID to 30000
    - enable automatic GC with 7-day deletion threshold
    - configure trusted users (root, ${username})
    - set download buffer size to 500 MB
    - enable nix-command and flakes experimental features
    - enable automatic store optimisation

--- modules/parts/darwin.nix
+++ modules/parts/darwin.nix
@@ -2,9 +2,27 @@
   flake.modules = {
     darwin.common = {username, ...}: {
       programs.zsh.enable = true;
+      ids.gids.nixbld = 30000;
+
       users.users.${username} = {
         home = "/Users/${username}";
         uid = 501;
+      };
+
+      nix = {
+        gc = {
+          automatic = true;
+          options = "--delete-older-than 7d";
+        };
+        settings = {
+          trusted-users = [
+            "root"
+            "${username}"
+          ];
+          download-buffer-size = 524288000;
+          experimental-features = ["nix-command" "flakes"];
+          auto-optimise-store = true;
+        };
       };
     };
 
