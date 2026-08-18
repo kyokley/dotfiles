0202f756e93e7a3fc5c10036de3f1db843e5099b
Kevin Yokley <kyokley@mars>
2026-08-18 06:10:49 -0500

    ♻️ refactor(main)(games): use centralized gameDir variable for ROM paths
    - add gameDir variable to centralize game directory path
    - update n64 binary reference to use full path with /bin/n64
    - replace hardcoded home directory paths with gameDir variable

--- modules/hosts/mars/games.nix
+++ modules/hosts/mars/games.nix
@@ -4,6 +4,7 @@
     config,
     ...
   }: let
+    gameDir = "${gameDir}";
     n64 = pkgs.writeShellApplication {
       name = "n64";
       text = ''
@@ -13,7 +14,7 @@
     mkN64Game = attrs: (pkgs.writeShellApplication {
       inherit (attrs) name;
       text = ''
-        n64 --fullscreen ${attrs.rom}
+        ${n64}/bin/n64 --fullscreen ${attrs.rom}
       '';
     });
   in {
@@ -27,23 +28,23 @@
       ++ (map mkN64Game [
         {
           name = "goldeneye";
-          rom = "${config.home.homeDirectory}/Games/goldeneye-007/Msftug41.v64";
+          rom = "${gameDir}/goldeneye-007/Msftug41.v64";
         }
         {
           name = "starfox";
-          rom = "${config.home.homeDirectory}/Games/N64/Starfox_64_USA-MSFTUG/Msftug34.v64";
+          rom = "${gameDir}/N64/Starfox_64_USA-MSFTUG/Msftug34.v64";
         }
         {
           name = "tetrisphere";
-          rom = "${config.home.homeDirectory}/Games/tetrisphere/Tetris.v64";
+          rom = "${gameDir}/tetrisphere/Tetris.v64";
         }
         {
           name = "hockey";
-          rom = "${config.home.homeDirectory}/Games/wayne-gretzkys-3d-hockey/As-wg3dh.v64";
+          rom = "${gameDir}/wayne-gretzkys-3d-hockey/As-wg3dh.v64";
         }
         {
           name = "wcw";
-          rom = "${config.home.homeDirectory}/Games/wcwnwo-revenge/AGS-WCWR.V64";
+          rom = "${gameDir}/wcwnwo-revenge/AGS-WCWR.V64";
         }
       ]);
   };
