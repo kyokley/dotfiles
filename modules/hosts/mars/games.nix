5700eb4a91f498b26204b9138410b2c6ced1e8de
Kevin Yokley <kyokley@mars>
2026-08-17 22:04:26 -0500

    🐛 fix(main)(games): update starfox 64 rom path
    - update directory from Starfox_64-MSFTUG to Starfox_64_USA-MSFTUG
    - update rom file from Msftug30.v64 to Msftug34.v64

--- modules/hosts/mars/games.nix
+++ modules/hosts/mars/games.nix
@@ -31,7 +31,7 @@
         }
         {
           name = "starfox";
-          rom = "${config.home.homeDirectory}/Games/Nintendo64-N64.SCENE.ARCHIVE.-.COMPLETE/Starfox_64-MSFTUG/Msftug30.v64";
+          rom = "${config.home.homeDirectory}/Games/Nintendo64-N64.SCENE.ARCHIVE.-.COMPLETE/Starfox_64_USA-MSFTUG/Msftug34.v64";
         }
       ]);
   };
