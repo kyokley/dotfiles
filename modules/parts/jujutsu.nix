5c2dfd947e1d4cf4f5670f456e8d414657cadc20
Kevin Yokley <yokley@dioxygen>
2026-09-14 06:30:26 -0500

    feat(jujutsu): enable jjui program

--- modules/parts/jujutsu.nix
+++ modules/parts/jujutsu.nix
@@ -11,12 +11,31 @@
       lazyjj
       jjui
     ];
-    programs.jujutsu = {
-      enable = true;
-      settings = {
-        user = {
-          email = lib.mkDefault "${username}@${hostName}";
-          name = lib.mkDefault fullName;
+    programs = {
+      jujutsu = {
+        enable = true;
+        settings = {
+          user = {
+            email = lib.mkDefault "${username}@${hostName}";
+            name = lib.mkDefault fullName;
+          };
+        };
+      };
+      jjui = {
+        enable = true;
+        settings = {
+          bindings = [
+            {
+              key = "ctrl+j";
+              action = "ui.preview_scroll_down";
+              scope = "ui.preview";
+            }
+            {
+              key = "ctrl+k";
+              action = "ui.preview_scroll_up";
+              scope = "ui.preview";
+            }
+          ];
         };
       };
     };
