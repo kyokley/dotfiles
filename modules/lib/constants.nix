fd6838cebb99d4d38ac61af7de8a9486c4571b47
Kevin Yokley <yokley@mars>
2026-09-14 09:59:16 -0500

    Update saturn ips

--- modules/lib/constants.nix
+++ modules/lib/constants.nix
@@ -5,6 +5,6 @@
     aarch64_darwin = "aarch64-darwin";
     x86_linux = "x86_64-linux";
   };
-  saturn-ip = "192.168.50.126";
-  saturn-wifi-ip = "192.168.50.96";
+  saturn-ip = "192.168.0.182";
+  saturn-wifi-ip = "192.168.0.96";
 }
